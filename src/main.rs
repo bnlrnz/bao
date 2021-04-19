c96a158c10b4e9a3991021b6f6dc1fb4926f01a2
bnlrnz <lorenzb@tu-freiberg.de>
2021-04-19 20:43:34 +0200

    reworked player management

--- src/main.rs
+++ src/main.rs
@@ -14,39 +14,43 @@ enum Mode {
 }
 
 #[derive(Copy, Clone, PartialEq)]
-enum Player{
-    PLAYER1,
-    PLAYER2,
+enum PlayerAgent{
+    HUMAN,
+    AI,
 }
 
-struct Board{
-    player1: [u8; 16],
-    player2: [u8; 16],
+struct Player{
+    name: &'static str,
+    agent: PlayerAgent,
+    board_half: [u8; 16],
 }
 
-impl Board{
-    fn new() -> Self {
-        Self {
-            player1: [2; 16],
-            player2: [2; 16],
+impl Player{
+    fn new(name: &'static str, agent: PlayerAgent) -> Self{
+        Self{
+            name,
+            agent,
+            board_half: [2;16],
         }
     }
 }
 
 struct Game{
-    board: Board,
     direction: Direction,
     mode: Mode,
     turn: u64,
+    player1: Player,
+    player2: Player,
 }
 
 impl Game{
-    fn new(direction: Direction, mode: Mode) -> Self{
+    fn new(direction: Direction, mode: Mode, player1: Player, player2: Player) -> Self{
         Self{
-            board: Board::new(),
             direction,
             mode,
-            turn: 1
+            turn: 1,
+            player1,
+            player2,
         }
     }
 
@@ -60,73 +64,70 @@ impl Game{
         // the current player could not make a move or its loosing condition was met
         // therefore we skip the player to display the winner
         self.turn += 1;
-        match self.get_current_player() {
-            Player::PLAYER1 => print!("Player 1 won!"),
-            Player::PLAYER2 => print!("Player 2 won!"),
-        }
+        print!("Congratulation {}. You won!", self.get_current_player().name);
     }
 
     #[inline(always)]
-    fn get_current_player(&self) -> Player {
+    fn get_current_and_opponent_player(&mut self) -> (&mut Player, &mut Player){
         match self.turn % 2 {
-            1 => Player::PLAYER1,
-            0 => Player::PLAYER2,
-            _ => panic!("dafuq could dis happen?"),
+            1 => (&mut self.player1, &mut self.player2),
+            0 => (&mut self.player2, &mut self.player1),
+            _ => unreachable!(),
         }
     }
 
     #[inline(always)]
-    fn get_current_player_and_oppenent_array(&mut self) -> (&mut [u8], &mut [u8]){
-        match self.get_current_player(){
-            Player::PLAYER1 => return (&mut self.board.player1, &mut self.board.player2),
-            Player::PLAYER2 => return (&mut self.board.player2, &mut self.board.player1),
+    fn get_current_player(&self) -> &Player {
+        match self.turn % 2 {
+            1 => &self.player1,
+            2 => &self.player2,
+            _ => unreachable!(),
         }
     }
 
     fn make_move(&mut self) {
-        let direction = self.direction;
-        let mode = self.mode;
-        let current_player = self.get_current_player();
-        let (player, opponent) = self.get_current_player_and_oppenent_array();
+        let (player, opponent) = self.get_current_and_opponent_player();
+        let mut player_board = player.board_half;
+        let mut opponent_board = opponent.board_half;
 
         let mut index= 0;
         let mut valid_index = false;
         while valid_index == false {
-            println!("{} enter bowl index: ", if current_player == Player::PLAYER1 {"Player 1"} else {"Player 2"});
+            println!("{} enter bowl index: ", player.name);
             
             index = Self::read_index();
             
-            if (0..16).contains(&index) && player[index] >= 2 {
+            if (0..16).contains(&index) && player_board[index] >= 2 {
                 valid_index = true;
             }else{
                 print!("Please enter a valid index (0-15). Bowl must contain at least 2 stones.");
             }
         }
 
-        let mut hand: u8 = player[index];
-        player[index] = 0;
+        let mut hand: u8 = player_board[index];
+        player_board[index] = 0;
 
         while hand > 0 {
-            index = Self::next_index(index, current_player, direction);
+            index = self.next_index(index);
             hand = hand - 1;
-            player[index] = player[index] + 1;
+            player_board[index] = player_board[index] + 1;
 
-            if hand == 0 && player[index] >= 2 {
-                hand = player[index];
-                player[index] = 0;
+            if hand == 0 && player_board[index] >= 2 {
+                hand = player_board[index];
+                player_board[index] = 0;
 
                 // steal from opponent
                 if index >= 8 && index <= 15 {
-                    hand = hand + match mode {
+                    hand = hand + match self.mode {
                         Mode::EASY => {
-                            let steal = opponent[index];
-                            opponent[index] = 0;
+                            let steal = opponent_board[index];
+                            opponent_board[index] = 0;
                             steal
                         },
                         Mode::NORMAL =>{
-                            let steal = opponent[index] + opponent[index-7];
-                            opponent[index] = 0;
-                            opponent[index-7] = 0;
+                            let steal = opponent_board[index] + opponent_board[index-7];
+                            opponent_board[index] = 0;
+                            opponent_board[index-7] = 0;
                             steal
                         },
                     }
@@ -136,21 +137,22 @@ impl Game{
     }
 
     #[inline(always)]
-    fn next_index(index: usize, player: Player, direction: Direction) -> usize {
+    fn next_index(&self, index: usize) -> usize {
         // I should use a ring for each player
-        match player {
-            Player::PLAYER1 => {
-                match direction {
+        match self.turn % 2 {
+            1 => {
+                match self.direction {
                     Direction::CW => if index+1 > 15 {0} else {index+1},
                     Direction::CCW => if index < 1 {15} else {index-1},
                 }
             },
-            Player::PLAYER2 => {
-                match direction {
+            0 => {
+                match self.direction {
                     Direction::CW => if index < 1 {15} else {index-1},
                     Direction::CCW => if index+1 > 15 {0} else {index+1},
                 }
             },
+            _ => panic!("How did we get here?`")
         }
     }
 
@@ -175,11 +177,9 @@ impl Game{
     }
 
     fn print_board(&self){
-        let player = self.get_current_player();
-
         println!("           {:2}Player 2{:2}",
-        if player == Player::PLAYER2 {"->"} else {""},
-        if player == Player::PLAYER2 {"<-"} else {""},
+        if self.turn % 2 == 0 {"->"} else {""},
+        if self.turn % 2 == 0  {"<-"} else {""},
         );
         print!("|");
         for i in 0..8 {
@@ -189,7 +189,7 @@ impl Game{
         println!("-----------------------------------------");
         print!("|");
         for i in 0..8 {
-            print!(" {:2} |", self.board.player2[i]);
+            print!(" {:2} |", self.player2.board_half[i]);
         }
         println!("");
 
@@ -202,15 +202,15 @@ impl Game{
         println!("-----------------------------------------");
         print!("|");
         for i in (8..16).rev() {
-            print!(" {:2} |", self.board.player2[i]);
+            print!(" {:2} |", self.player2.board_half[i]);
         }
-        println!(" Stones: {}", self.board.player2.iter().sum::<u8>());
+        println!(" Stones: {}", self.player2.board_half.iter().sum::<u8>());
         println!("==================================================== Round: {}", self.turn);
         print!("|");
         for i in (8..16).rev() {
-            print!(" {:2} |", self.board.player1[i]);
+            print!(" {:2} |", self.player1.board_half[i]);
         }
-        println!(" Stones: {}", self.board.player1.iter().sum::<u8>());
+        println!(" Stones: {}", self.player1.board_half.iter().sum::<u8>());
         println!("-----------------------------------------");
         print!("|");
         for i in (8..16).rev() {
@@ -220,7 +220,7 @@ impl Game{
         println!("-----------------------------------------");
         print!("|");
         for i in 0..8 {
-            print!(" {:2} |", self.board.player1[i]);
+            print!(" {:2} |", self.player1.board_half[i]);
         }
         println!("");
         println!("-----------------------------------------");
@@ -230,45 +230,28 @@ impl Game{
         }
         println!("");
         println!("           {:2}Player 1{:2}",
-         if player == Player::PLAYER1 {"->"} else {""},
-         if player == Player::PLAYER1 {"<-"} else {""},
+         if self.turn % 2 == 1  {"->"} else {""},
+         if self.turn % 2 == 1  {"<-"} else {""},
         );
     }
 
     fn move_possible(&self) -> bool{
-        match self.get_current_player() {
-            Player::PLAYER1 => {
-                for bowl in self.board.player1.iter() {
-                    if bowl >= &2 {
-                        return true
-                    }
-                }
-                println!("Player 1: no possible move left :(");
-                false
-            },
-            Player::PLAYER2 => {
-                for bowl in self.board.player2.iter() {
-                    if bowl >= &2 {
-                        return true
-                    }
-                }
-                println!("Player 2: no possible move left :(");
-                false
-            },
+        for bowl in self.get_current_player().board_half.iter(){
+            if bowl >= &2 {
+                return true
+            }
         }
+        print!("{}: no possible moveds left :(", self.get_current_player().name);
+        false
     }
 
     fn game_over(&self)->bool{
-        let current_player_array =
-            match self.get_current_player() {
-                Player::PLAYER1 => self.board.player1,
-                Player::PLAYER2 => self.board.player2,
-            };
+        let current_player_board = self.get_current_player().board_half;
 
         match self.mode {
             Mode::EASY => {
                 for bowl in 8..15 {
-                    if current_player_array[bowl] != 0 {
+                    if current_player_board[bowl] != 0 {
                         return false
                     }
                 }
@@ -276,7 +259,7 @@ impl Game{
             },
             Mode::NORMAL => {
                 for bowl in 0..15 {
-                    if current_player_array[bowl] != 0 {
+                    if current_player_board[bowl] != 0 {
                         return false
                     }
                 }
@@ -287,5 +270,8 @@ impl Game{
 }
 
 fn main(){
-    Game::new(Direction::CW, Mode::EASY).run();
+    let mut game = Game::new(Direction::CW, Mode::EASY,
+         Player::new("Player 1", PlayerAgent::HUMAN),
+         Player::new("Player 2", PlayerAgent::HUMAN));
+    game.run();
 }

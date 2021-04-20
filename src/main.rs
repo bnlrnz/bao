1c8191a687868e19a285f7931d6236185e7886a4
bnlrnz <lorenzb@tu-freiberg.de>
2021-04-20 10:58:43 +0200

    reworking gamelogic

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,20 @@ use std::print;
 use std::io;
 
 #[derive(Copy, Clone)]
+#[allow(unused)]
 enum Direction {
     CW,
     CCW,
 }
 #[derive(Copy, Clone)]
+#[allow(unused)]
 enum Mode {
     NORMAL,// all stones required
     EASY,  // just the inner row must be empty to win
 }
 
 #[derive(Copy, Clone, PartialEq)]
+#[allow(unused)]
 enum PlayerAgent{
     HUMAN,
     AI,
@@ -23,6 +26,7 @@ struct Player{
     name: &'static str,
     agent: PlayerAgent,
     board_half: [u8; 16],
+    get_index_fn: fn() -> usize,
 }
 
 impl Player{
@@ -31,14 +35,35 @@ impl Player{
             name,
             agent,
             board_half: [2;16],
+            get_index_fn: Self::read_index,
         }
+    }
+
+    fn read_index() -> usize{
+        let mut index: Option<usize> = None;
+        while index == None {
+            let mut input_text = String::new();
+            io::stdin()
+                .read_line(&mut input_text)
+                .expect("failed to read from stdin");
+            
+            let trimmed = input_text.trim();
+            match trimmed.parse::<usize>() {
+                Ok(i) => return i,
+                Err(..) => {
+                    println!("Please enter a valid number!");
+                    index = None
+                },
+            };
+        }
+        index.unwrap()
     }
 }
 
 struct Game{
     direction: Direction,
     mode: Mode,
-    turn: u64,
+    turn: usize,
     player1: Player,
     player2: Player,
 }
@@ -56,7 +81,9 @@ impl Game{
 
     fn run(&mut self) {
         while self.move_possible() && !self.game_over(){
-            self.print_board();
+            if self.get_current_player().agent == PlayerAgent::HUMAN{
+                self.print_board();
+            }
             self.make_move();
             self.turn += 1;
         }
@@ -80,54 +107,56 @@ impl Game{
     fn get_current_player(&self) -> &Player {
         match self.turn % 2 {
             1 => &self.player1,
-            2 => &self.player2,
+            0 => &self.player2,
             _ => unreachable!(),
         }
     }
 
     fn make_move(&mut self) {
+        let turn = self.turn;
+        let direction = self.direction;
+        let mode = self.mode;
+
         let (player, opponent) = self.get_current_and_opponent_player();
-        let mut player_board = player.board_half;
-        let mut opponent_board = opponent.board_half;
 
         let mut index= 0;
         let mut valid_index = false;
         while valid_index == false {
             println!("{} enter bowl index: ", player.name);
             
-            index = Self::read_index();
+            index = (player.get_index_fn)();
             
-            if (0..16).contains(&index) && player_board[index] >= 2 {
+            if (0..16).contains(&index) && player.board_half[index] >= 2 {
                 valid_index = true;
             }else{
                 print!("Please enter a valid index (0-15). Bowl must contain at least 2 stones.");
             }
         }
 
-        let mut hand: u8 = player_board[index];
-        player_board[index] = 0;
+        let mut hand: u8 = player.board_half[index];
+        player.board_half[index] = 0;
 
         while hand > 0 {
-            index = self.next_index(index);
+            index = Self::next_index(index, direction, turn);
             hand = hand - 1;
-            player_board[index] = player_board[index] + 1;
+            player.board_half[index] = player.board_half[index] + 1;
 
-            if hand == 0 && player_board[index] >= 2 {
-                hand = player_board[index];
-                player_board[index] = 0;
+            if hand == 0 && player.board_half[index] >= 2 {
+                hand = player.board_half[index];
+                player.board_half[index] = 0;
 
                 // steal from opponent
                 if index >= 8 && index <= 15 {
-                    hand = hand + match self.mode {
+                    hand = hand + match mode {
                         Mode::EASY => {
-                            let steal = opponent_board[index];
-                            opponent_board[index] = 0;
+                            let steal = opponent.board_half[index];
+                            opponent.board_half[index] = 0;
                             steal
                         },
                         Mode::NORMAL =>{
-                            let steal = opponent_board[index] + opponent_board[index-7];
-                            opponent_board[index] = 0;
-                            opponent_board[index-7] = 0;
+                            let steal = opponent.board_half[index] + opponent.board_half[index-7];
+                            opponent.board_half[index] = 0;
+                            opponent.board_half[index-7] = 0;
                             steal
                         },
                     }
@@ -137,43 +166,23 @@ impl Game{
     }
 
     #[inline(always)]
-    fn next_index(&self, index: usize) -> usize {
+    fn next_index(index: usize, direction: Direction, turn: usize) -> usize {
         // I should use a ring for each player
-        match self.turn % 2 {
+        match turn % 2 {
             1 => {
-                match self.direction {
+                match direction {
                     Direction::CW => if index+1 > 15 {0} else {index+1},
                     Direction::CCW => if index < 1 {15} else {index-1},
                 }
             },
             0 => {
-                match self.direction {
+                match direction {
                     Direction::CW => if index < 1 {15} else {index-1},
                     Direction::CCW => if index+1 > 15 {0} else {index+1},
                 }
             },
-            _ => panic!("How did we get here?`")
+            _ => unreachable!(),
         }
-    }
-
-    fn read_index() -> usize{
-        let mut index: Option<usize> = None;
-        while index == None {
-            let mut input_text = String::new();
-            io::stdin()
-                .read_line(&mut input_text)
-                .expect("failed to read from stdin");
-            
-            let trimmed = input_text.trim();
-            match trimmed.parse::<usize>() {
-                Ok(i) => return i,
-                Err(..) => {
-                    println!("Please enter a valid number!");
-                    index = None
-                },
-            };
-        }
-        index.unwrap()
     }
 
     fn print_board(&self){
@@ -273,5 +282,10 @@ fn main(){
     let mut game = Game::new(Direction::CW, Mode::EASY,
          Player::new("Player 1", PlayerAgent::HUMAN),
          Player::new("Player 2", PlayerAgent::HUMAN));
+        
+    // game.player2.get_index_fn = ||{
+    //     4
+    // };
+
     game.run();
 }
